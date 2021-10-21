fda699d9ff1cc9e41483ed48e40a12b206f4ce2c
Chase Fleming <chaseefleming@gmail.com>
2021-10-21 16:39:35 -0700

    Create services API

---
+++ pages/api/services.js
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import servicesJson from '../../data/services.json'
+
+export default function handler(req, res) {
+  const { network } = req.query
+  const validNetworks = ['mainnet', 'testnet', 'canarynet']
+  const networkStr = (network || 'mainnet').toLowerCase()
+  const services = servicesJson[networkStr]
+  
+  if (!validNetworks.includes(networkStr)) {
+    return res.status(400).json({ message: 'Invalid Network' })
+  }
+
+  return res.status(200).json(services)
+}
