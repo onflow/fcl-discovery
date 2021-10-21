2d4df09f772a4c9a001e0fdc528f8c526be78da3
Chase Fleming <chaseefleming@gmail.com>
2021-10-21 16:17:34 -0700

    Create next app

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return <div>FCL Discovery</div>
+}
