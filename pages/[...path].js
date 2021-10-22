6615c42e24f5489faacedcb8fc7403da3eed9fdf
Chase Fleming <chaseefleming@gmail.com>
2021-10-22 14:48:11 -0700

    Break into services component

--- pages/[...path].js
+++ pages/[...path].js
@@ -3,23 +3,28 @@ import { useFetch } from '../hooks/useFetch'
 import { PATHS } from '../helpers/constants'
 import { createPathFromArray } from '../helpers/paths'
 
+const Services = ({ network }) => {
+  const requestUrl = `/api/services?=${network}`
+  const { loading, data, error } = useFetch(requestUrl)
+
+  if (loading) return <div>Loading...</div>
+  if (error) return <div>Error Fetching Services</div>
+
+  return (
+    <div>{JSON.stringify(data)}</div>
+  )
+}
+
 const Discovery = () => {
   const router = useRouter()
   const { path } = router.query // ['authn'] ['testnet', 'authn'] ['canarynet', 'authn']
   const pathStr = createPathFromArray(path)
   const isValidRoute = Object.values(PATHS).some(p => p === pathStr)
   const network = path && path.length === 2 ? path[0] : 'mainnet'
-  const requestUrl = isValidRoute ? `/api/services?=${network}` : null
-
-  const { loading, data, error } = useFetch(requestUrl)
 
   if (!isValidRoute) return <div>Page Not Found</div>
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error Fetching Services</div>
   
-  return (
-    <div>{JSON.stringify(data)}</div>
-  )
+  return <Services network={network} />
 }
 
 export default Discovery
