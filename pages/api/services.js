a1488544ded7e941e77edbf308a4a19b54437824
Chase Fleming <chaseefleming@gmail.com>
2021-11-01 14:43:51 -0700

    Setup cors for api route

--- pages/api/services.js
+++ pages/api/services.js
@@ -1,7 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors"
 import servicesJson from "../../data/services.json"
 
-export default function handler(req, res) {
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET"]
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
+
   const {network} = req.query
   const validNetworks = ["mainnet", "testnet", "canarynet"]
   const networkStr = (network || "mainnet").toLowerCase()
