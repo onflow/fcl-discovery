080ed8057d86fc0b269db63cb0fe8f040cdcd94e
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-05-18 14:09:04 -0700

    Add Explainer component test

--- components/__tests__/Explainer.test.js
+++ components/__tests__/Explainer.test.js
@@ -1,29 +1,35 @@
 import { render } from '@testing-library/react'
 import Explainer from '../Explainer'
-import * as hooks from '../../hooks'
+import * as hooks from '../../hooks/useFCL'
 
 describe('Component: Explainer', () => {
+  let fclHookSpy
+
+  beforeEach(() => {
+    fclHookSpy = jest.spyOn(hooks, 'useFCL')
+  })
+
+  afterEach(() => {
+    fclHookSpy.mockRestore()
+  })
+
   test('should render the component with app title', () => {
-    jest.spyOn(hooks, 'useFCL').mockImplementation(() => {
-      return {
-        appConfig: {
-          title: 'Test App',
-        },
-      }
-    })
+    fclHookSpy.mockImplementation(() => ({
+      appConfig: {
+        title: 'Test App',
+      },
+    }))
 
     const { container } = render(<Explainer />)
     expect(container.firstChild).toMatchSnapshot()
   })
 
   test('should render app as unknown in Explainer component if no title', () => {
-    jest.spyOn(hooks, 'useFCL').mockImplementation(() => {
-      return {
-        appConfig: {
-          title: null,
-        },
-      }
-    })
+    fclHookSpy.mockImplementation(() => ({
+      appConfig: {
+        title: null,
+      },
+    }))
 
     const { container } = render(<Explainer />)
     expect(container.firstChild).toMatchSnapshot()
