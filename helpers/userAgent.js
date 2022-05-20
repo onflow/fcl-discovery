fd9635b9cc966379e13e82ed759d5dc075b931bd
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-05-20 13:13:23 -0700

    Get platform

--- helpers/userAgent.js
+++ helpers/userAgent.js
@@ -1,1 +1,11 @@
-export const hasUserAgent = type => window.navigator.userAgent.includes(type)
+import { USER_AGENTS } from './constants'
+
+export const hasUserAgent = type => window?.navigator?.userAgent.includes(type)
+
+export const getPlatform = () => {
+  for (const value of Object.values(USER_AGENTS)) {
+    if (hasUserAgent(value)) {
+      return value
+    }
+  }
+}
