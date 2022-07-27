39e84a1e6102fc39804033cad9f2d1db38e4ae60
Chase Fleming <chasefleming@users.noreply.github.com>
2022-07-27 16:37:44 -0700

    Bug: Check if local storage is available (#78)
    Co-authored-by: Chase Fleming <1666730+chasefleming@users.noreply.github.com>

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,13 +1,25 @@
 import { useState, useEffect } from 'react'
 
-function getStorageValue(key, defaultValue) {
+// Local storage not supported in incognito and checking window object not sufficient
+const isLocalStorageAvailable = () => {
+  const testString = 'test'
+  try {
+    localStorage.setItem(testString, testString)
+    localStorage.removeItem(testString)
+    return true
+  } catch (e) {
+    return false
+  }
+} 
+
+const getStorageValue = (key, defaultValue) => {
   const saved = localStorage.getItem(key)
   const initial = JSON.parse(saved)
   return initial || defaultValue
 }
 
 export const useLocalStorage = (key, defaultValue) => {
-  if (!window.localStorage) {
+  if (!isLocalStorageAvailable()) {
     return [null, () => {}]
   }
 
