81b7dccab0c131b093202c61a6868bd423fe6c8f
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-10-27 12:28:44 -0700

    Create explicit mainnet path

--- helpers/__tests__/paths.test.js
+++ helpers/__tests__/paths.test.js
@@ -3,30 +3,35 @@ import { createPathFromArray, isValidPath, getNetworkFromPath } from '../paths'
 describe('paths helpers: createPathFromArray', () => {
   it('should create paths from array of directories', () => {
     const arrOne = ['authn']
-    const arrTwo = ['testnet', 'authn']
-    const arrThree = ['canarynet', 'authn']
+    const arrTwo = ['mainnet', 'authn']
+    const arrThree = ['testnet', 'authn']
+    const arrFour = ['canarynet', 'authn']
 
     const expectedResponseOne = '/authn'
-    const expectedResponseTwo = '/testnet/authn'
-    const expectedResponseThree = '/canarynet/authn'
+    const expectedResponseTwo = '/mainnet/authn'
+    const expectedResponseThree = '/testnet/authn'
+    const expectedResponseFour = '/canarynet/authn'
 
     expect(createPathFromArray(arrOne)).toEqual(expectedResponseOne)
     expect(createPathFromArray(arrTwo)).toEqual(expectedResponseTwo)
     expect(createPathFromArray(arrThree)).toEqual(expectedResponseThree)
+    expect(createPathFromArray(arrFour)).toEqual(expectedResponseFour)
   })
 })
 
 describe('paths helpers: isValidPath', () => {
   it('should check a path is valid', () => {
     const pathOne = ['authn']
-    const pathTwo = ['testnet', 'authn']
-    const pathThree = ['canarynet', 'authn']
-    const pathFour = ['foo', 'bar']
+    const pathTwo = ['mainnet', 'authn']
+    const pathThree = ['testnet', 'authn']
+    const pathFour = ['canarynet', 'authn']
+    const pathFive = ['foo', 'bar']
 
     expect(isValidPath(pathOne)).toBe(true)
     expect(isValidPath(pathTwo)).toBe(true)
     expect(isValidPath(pathThree)).toBe(true)
-    expect(isValidPath(pathFour)).toBe(false)
+    expect(isValidPath(pathFour)).toBe(true)
+    expect(isValidPath(pathFive)).toBe(false)
     expect(isValidPath(null)).toBe(false)
   })
 })
