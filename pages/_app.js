644ed0f0401115fed867c175c718864271274365
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-01-19 15:02:26 -0800

    Convert app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,95 +1,37 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 import { Message } from '../components/Message'
 import { NETWORKS, SUPPORTED_VERSIONS } from '../helpers/constants'
 import { getNetworkFromPath } from '../helpers/paths'
 import { isGreaterThanOrEqualToVersion } from '../helpers/version'
 import { handleCancel } from '../helpers/window'
 import { useFCL } from '../hooks/useFCL'
+import { ChakraProvider } from '@chakra-ui/react'
+import {
+  extendTheme,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
 
-const GlobalStyle = createGlobalStyle`
-  * { 
-    margin:0;
-    padding:0;
-    font-variant-ligatures: common-ligatures;
-  }
-  :root {
-    --text-primary: #0F0F0F;
-    --text-secondary: #0B0B0B;
-    --font-family: Overpass, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif;
-  }
-  body {
-    background-color: transparent;
-    color: var(--text-primary);
-    font-family: var(--font-family);
-    font-size: 16px;
-    line-height: 22px;
-  }
-`
-const Wrapper = styled.div`
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
+const theme = extendTheme({
+  styles: {
+    global: {
+      'body': {
+        backgroundColor: 'transparent'
+      },
+    },
+  },
+})
 
 const MessageAnchor = styled.div`
   position: absolute;
   bottom: 0;
   right: 20px;
   left: 20px;
-`
-
-const Inner = styled.div`
-  position: relative;
-  max-height: 100vh;
-  max-width: 100vw;
-  width: 30rem;
-  padding: 1rem;
-  box-sizing: border-box;
-  border-radius: 0.5rem;
-  background-color: white;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  overflow-y: auto;
-`
-
-const CloseSection = styled.div`
-  position: absolute;
-  cursor: pointer;
-  width: 20px;
-  height: 20px;
-  right: 25px;
-  transition: 0.1s ease-in transform;
-  animation-name: waitToShow;
-  animation-duration: 1s;
-
-  &:hover {
-    transform: scale(1.1);
-  }
-
-  @keyframes waitToShow {
-    0% {
-      opacity: 0;
-    }
-    80% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-  }
-`
-
-const CloseIcon = styled.img`
-  width: 20px;
-  height: 20px;
 `
 
 function MyApp({ Component, pageProps }) {
@@ -117,24 +59,29 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <GlobalStyle />
-      <Wrapper onClick={handleCancel}>
-        <Inner onClick={e => e.stopPropagation()}>
-          <CloseSection onClick={handleCancel}>
-            <CloseIcon src="/images/close.svg" alt="Close" />
-          </CloseSection>
-          <Component {...pageProps} />
-        </Inner>
-        {showDeveloperMessage && messageOpen && (
-          <MessageAnchor>
-            <Message
-              text={developerMessage}
-              link={developerLink}
-              onClose={closeMessage}
-            />
-          </MessageAnchor>
-        )}
-      </Wrapper>
+      <ChakraProvider theme={theme}>
+        <Modal isOpen={true} onClose={handleCancel} isCentered>
+          <ModalOverlay
+            bg='blackAlpha.300'
+            backdropFilter='blur(10px)'
+          />
+          <ModalContent>
+            <ModalCloseButton />
+            <ModalBody paddingTop={5} paddingBottom={5}>
+              <Component {...pageProps} />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </ChakraProvider>
+      {showDeveloperMessage && messageOpen && (
+        <MessageAnchor>
+          <Message
+            text={developerMessage}
+            link={developerLink}
+            onClose={closeMessage}
+          />
+        </MessageAnchor>
+      )}
     </>
   )
 }
