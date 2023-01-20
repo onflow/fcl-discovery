b0a8e00a36962cd10635cff88309200c151bc085
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-01-20 10:22:31 -0800

    Convert flow header

--- components/Headers/FlowHeader.js
+++ components/Headers/FlowHeader.js
@@ -1,41 +1,21 @@
-import styled from 'styled-components'
-
-const AppHeader = styled.div`
-  margin-bottom: 2rem;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: wrap;
-`
-
-const AppLogoWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  margin-bottom: 8px;
-`
-
-const AppLogo = styled.img`
-  height: 3.5rem;
-`
-
-const AppTitle = styled.h2`
-  text-align: left;
-  color: #2a2825;
-  font-size: 1.2rem;
-`
+import { Center, Flex, Image, Text } from '@chakra-ui/react'
 
 export default function FlowHeader() {
   return (
-    <AppHeader>
-      <AppLogoWrapper>
-        <AppLogo src="/logo.svg" alt="Flow Logo" />
-      </AppLogoWrapper>
-      <AppTitle>Choose a Provider</AppTitle>
-    </AppHeader>
+    <>
+      <Center>
+        <Flex flexDirection="column" alignItems="center" marginBottom={4}>
+          <Image 
+            src="/logo.svg" alt="Logo"
+            w='140px'
+            h='auto'
+            marginBottom={3}
+          />
+          <Text color='grey'>
+            Choose a Provider
+          </Text>
+        </Flex>
+      </Center>
+    </>
   )
 }
