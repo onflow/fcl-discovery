570a4b026d9658a07a3e02d40a0b419b8eb6e16d
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-01-20 10:22:55 -0800

    Remove app header two

--- components/Headers/AppHeader.js
+++ components/Headers/AppHeader.js
@@ -1,55 +1,25 @@
-import styled from 'styled-components'
-import { COLORS } from '../../helpers/constants'
+import { Image, Text, Heading, HStack, Stack } from '@chakra-ui/react'
 import { useFCL } from '../../hooks/useFCL'
-
-const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 10px 0;
-`
-
-const AppLogo = styled.img`
-  height: 5rem;
-  width: 5rem;
-  border-radius: 10%;
-  overflow: hidden;
-`
-
-const DefaultImgWrapper = styled.div`
-  height: 5rem;
-  width: 5rem;
-  background: ${COLORS.GREY_LIGHTER_TWO};
-  border-radius: 10%;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const DefaultImg = styled.img`
-  height: 4rem;
-  width: 4rem;
-`
-
-const ClientDomain = styled.div`
-  color: ${COLORS.SECONDARY};
-  margin-top: 5px;
-`
 
 export default function AppHeader() {
   const { appConfig, clientConfig } = useFCL()
+  const title = appConfig?.title ? `Connect to ${appConfig?.title}`: 'Connect'
 
   return (
-    <HeaderContainer>
-      {appConfig?.icon ? (
-        <AppLogo src={appConfig.icon} alt="Logo" />
-      ) : (
-        <DefaultImgWrapper>
-          <DefaultImg src="/images/default-img.svg" />
-        </DefaultImgWrapper>
-      )}
-      <ClientDomain>{clientConfig?.hostname}</ClientDomain>
-    </HeaderContainer>
+    <Stack marginBottom={25}>
+      <Heading as='h2' size='md'>{title}</Heading>
+      <HStack>
+        {appConfig?.icon &&
+          <Image 
+            src={appConfig.icon} alt="Logo"
+            borderRadius={50}
+            boxSize='25px'
+          />
+        }
+        <Text color='grey'>
+          {clientConfig?.hostname || 'Unknown'}
+        </Text>
+      </HStack>
+    </Stack>
   )
 }
