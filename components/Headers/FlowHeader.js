166cfcbb42377f8455c7b32f6a3b3c640e694071
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-01-20 10:26:09 -0800

    Fix tests

--- components/Headers/FlowHeader.js
+++ components/Headers/FlowHeader.js
@@ -2,20 +2,18 @@ import { Center, Flex, Image, Text } from '@chakra-ui/react'
 
 export default function FlowHeader() {
   return (
-    <>
-      <Center>
-        <Flex flexDirection="column" alignItems="center" marginBottom={4}>
-          <Image 
-            src="/logo.svg" alt="Logo"
-            w='140px'
-            h='auto'
-            marginBottom={3}
-          />
-          <Text color='grey'>
-            Choose a Provider
-          </Text>
-        </Flex>
-      </Center>
-    </>
+    <Center>
+      <Flex flexDirection="column" alignItems="center" marginBottom={4}>
+        <Image 
+          src="/logo.svg" alt="Logo"
+          w='140px'
+          h='auto'
+          marginBottom={3}
+        />
+        <Text color='grey'>
+          Choose a Provider
+        </Text>
+      </Flex>
+    </Center>
   )
 }
