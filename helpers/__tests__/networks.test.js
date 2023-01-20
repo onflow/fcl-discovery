1b186c135fc781c7335cc16eea0dfaa64a9c5242
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-01-20 11:08:35 -0800

    Add test for networks

---
+++ helpers/__tests__/networks.test.js
@@ -0,0 +1,33 @@
+import { isTestnet } from "../networks"
+
+describe('Helpers: networks', () => {
+  let windowSpy
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get')
+  })
+
+  afterEach(() => {
+    windowSpy.mockRestore()
+  })
+
+  test('should check for testnet', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        pathname: '/testnet/authn',
+      }
+    }))
+
+    expect(isTestnet()).toBe(true)
+  })
+
+  test('should fail if not testnet', () => {
+    windowSpy.mockImplementation(() => ({
+      location: {
+        pathname: '/mainnet/authn',
+      }
+    }))
+
+    expect(isTestnet()).toBe(false)
+  })
+})
