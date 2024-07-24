b01b58ff08d63784437793d847206023dcad910e
Jordan Ribbink <jribbink@telus.net>
2024-07-24 10:22:51 -0700

    remove provider bug

--- pages/api/[network]/wallets.ts
+++ pages/api/[network]/wallets.ts
@@ -1,17 +1,10 @@
-import { map } from 'rambda'
 import Sentry from '../../../config/sentry.server'
 import { cors, getWalletsFromRequest, runMiddleware } from './_common'
 
 async function handler(req, res) {
   await runMiddleware(req, res, cors)
 
-  const discoveryWallets = await getWalletsFromRequest(req, {
-    additionalServicesPipeFactory: () =>
-      map(({ provider: _, ...srv }) => ({
-        ...srv,
-        provider: undefined as any,
-      })),
-  })
+  const discoveryWallets = await getWalletsFromRequest(req)
   return res.status(200).json(discoveryWallets)
 }
 
