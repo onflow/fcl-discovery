b6ecf4dfa6bb2cbd90340005d5dc622ecea25907
Jordan Ribbink <jribbink@telus.net>
2024-07-25 10:19:15 -0700

    remove dummy files

--- pages/api/foo/authn.ts
+++
@@ -1,8 +0,0 @@
-// Alias for /api/mainnet/authn endpoint
-import { NextApiRequest, NextApiResponse } from 'next'
-import originalHandler from '../[network]/authn'
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  req.query.network = 'mainnet'
-  req.url = '/api/mainnet/authn'
-  return originalHandler(req, res)
-}
--- pages/api/foo/wallets.ts
+++
@@ -1,8 +0,0 @@
-// Alias for /api/mainnet/wallets endpoint
-import { NextApiRequest, NextApiResponse } from 'next'
-import originalHandler from '../[network]/wallets'
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  req.query.network = 'mainnet'
-  req.url = '/api/mainnet/wallets'
-  return originalHandler(req, res)
-}
