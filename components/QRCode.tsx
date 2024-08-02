94c35f73b27819243f5752acd6d6c4c659d3f6f0
Jordan Ribbink <jribbink@telus.net>
2024-08-01 20:27:15 -0700

    It actually looks nice :)

--- components/QRCode.tsx
+++ components/QRCode.tsx
@@ -1,47 +1,119 @@
-import { Box } from '@chakra-ui/react'
-import { ComponentProps, memo, useMemo } from 'react'
+import { Box, Image } from '@chakra-ui/react'
+import { memo } from 'react'
 import QRCodeLib from 'qrcode'
+import NextImage, { StaticImageData } from 'next/image'
 
 type QRCodeProps = {
   value: string
-  size: number
-} & ComponentProps<typeof Box>
+  image?: string | StaticImageData
+  size?: string
+  imageSize?: string
+}
 
-const QRCode = memo(function QRCode({ value, size, ...props }: QRCodeProps) {
-  const flatModules = QRCodeLib.create(value, {
+const QRCode = memo(function QRCode({
+  value,
+  image,
+  size,
+  imageSize,
+}: QRCodeProps) {
+  const qr = QRCodeLib.create(value, {
     errorCorrectionLevel: 'H',
-  }).modules.data
+  })
+  const modules1D = qr.modules.data
 
   const modules: boolean[][] = []
-  const length = Math.sqrt(flatModules.length)
+  const length = Math.sqrt(modules1D.length)
+
+  const isFindingPattern = (x: number, y: number) =>
+    (x < 8 && (y < 8 || y >= length - 8)) || (x >= length - 8 && y < 8)
 
   for (let i = 0; i < length; i++) {
     modules.push(
-      [...flatModules.slice(i * length, (i + 1) * length)].map(Boolean)
+      [...modules1D.slice(i * length, (i + 1) * length)].map((bit, j) => {
+        return bit && !isFindingPattern(i, j)
+      })
     )
   }
 
+  const findingPatternPositions = [
+    [0, 0],
+    [0, length - 7],
+    [length - 7, 0],
+  ]
+
   return (
-    <Box {...props}>
+    <Box position="relative" width={size} height={size}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
+        viewBox={`0 0 ${modules.length} ${modules.length}`}
         width={size}
         height={size}
-        viewBox={`0 0 ${modules.length} ${modules.length}`}
       >
         {modules.map((row, y) =>
           row.map((cell, x) => (
-            <rect
+            <circle
               key={`${x}-${y}`}
-              x={x}
-              y={y}
-              width={1}
-              height={1}
+              cx={x + 0.5}
+              cy={y + 0.5}
+              r={0.4}
               fill={cell ? 'black' : 'white'}
             />
           ))
         )}
+
+        {findingPatternPositions.map(([x, y]) => (
+          <>
+            <rect
+              key={`${x}-${y}`}
+              x={x}
+              y={y}
+              width={7}
+              height={7}
+              fill="black"
+              rx={2}
+              ry={2}
+            />
+
+            <rect
+              key={`${x}-${y}-inner`}
+              x={x + 1}
+              y={y + 1}
+              width={5}
+              height={5}
+              fill="white"
+              rx={1.5}
+              ry={1.5}
+            />
+
+            <rect
+              key={`${x}-${y}-inner2`}
+              x={x + 2}
+              y={y + 2}
+              width={3}
+              height={3}
+              fill="black"
+            />
+          </>
+        ))}
       </svg>
+      <Box
+        position="absolute"
+        top={`calc(50% - ${imageSize} / 2)`}
+        left={`calc(50% - ${imageSize} / 2)`}
+        width={imageSize}
+        height={imageSize}
+        bg="white"
+      >
+        <Image
+          as={NextImage}
+          src={image as any}
+          alt="QR Code"
+          layout="fill"
+          objectFit="contain"
+          boxSize={imageSize}
+          borderRadius="lg"
+        />
+      </Box>
     </Box>
   )
 })
--- components/views/ScanInstall.tsx
+++ components/views/ScanInstall.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Spacer, Stack, Text } from '@chakra-ui/react'
 import { Wallet } from '../../data/wallets'
 import QRCode from '../QRCode'
 
@@ -9,30 +9,36 @@ interface ScanInstallProps {
 
 export default function ScanInstall({ onContinue, wallet }: ScanInstallProps) {
   return (
-    <Stack flexGrow={1} flexShrink={1} alignItems="center">
+    <Stack
+      flexGrow={1}
+      flexShrink={1}
+      alignItems="center"
+      spacing="lg"
+      px="lg"
+      pb="lg"
+    >
       <Text color="gray" fontWeight="bold" maxW="2xs" textAlign="center">
         Scan with your phone to install on iOS or Android
       </Text>
 
-      <Box
-        padding={4}
+      <Flex
+        padding="md"
         bg="white"
         borderRadius="xl"
         shadow="md"
         border="1px"
         borderColor="gray.200"
       >
-        <QRCode level="M" value={wallet.installLink?.mobile} size={300} />
-      </Box>
+        <QRCode value={wallet.installLink?.mobile} size="18.75rem" />
+      </Flex>
 
       <Button
         variant="primary"
         size="sm"
-        mt="auto"
-        mb={4}
         onClick={onContinue}
         fontWeight="bold"
         borderRadius="full"
+        mt="auto"
       >
         Continue
       </Button>
