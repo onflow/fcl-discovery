ca691f8fbcdcef63de3e261c9f33344f314a0e3b
Jordan Ribbink <jribbink@telus.net>
2024-08-08 10:26:55 -0700

    update rpc reqs

--- helpers/rpc.ts
+++ helpers/rpc.ts
@@ -1,19 +1,18 @@
-import { RpcClient, RpcRequest } from "@onflow/util-rpc"
-import { Service } from "../types"
+import { RpcClient, RpcRequest } from '@onflow/util-rpc'
+import { Service } from '../types'
 
 export enum DiscoveryNotification {
-    NOTIFY_QRCODE_EXPIRY = 'notifyQRCodeExpiry',
-    NOTIFY_QRCODE_ERROR = 'notifyQRCodeError',
+  NOTIFY_QRCODE_ERROR = 'notifyQRCodeError',
 }
 
 export enum FclRequest {
-REQUEST_WALLETCONNECT_QRCODE = 'requestWalletConnectQRCode',
-EXEC_SERVICE = 'execService',
+  REQUEST_WALLETCONNECT_QRCODE = 'requestWalletConnectQRCode',
+  EXEC_SERVICE = 'execService',
 }
 
 export type FclRequests = {
-[FclRequest.REQUEST_WALLETCONNECT_QRCODE]: RpcRequest<{}, { uri: string }>
-[FclRequest.EXEC_SERVICE]: RpcRequest<{ service: Service }, {}>
+  [FclRequest.REQUEST_WALLETCONNECT_QRCODE]: RpcRequest<{}, { uri: string }>
+  [FclRequest.EXEC_SERVICE]: RpcRequest<{ service: Service }, {}>
 }
 
 export type FclRpcClient = RpcClient<FclRequests, {}>
