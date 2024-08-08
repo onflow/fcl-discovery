fa40a258385ec51b8db20cbb78628e6dab2b0886
Jordan Ribbink <jribbink@telus.net>
2024-08-08 15:14:48 -0700

    delete files

--- components/views/WalletSelection.tsx
+++ components/views/WalletSelection.tsx
@@ -1,21 +1,8 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  HStack,
-  Stack,
-  Text,
-} from '@chakra-ui/react'
+import { Button, Divider, HStack, Stack, Text } from '@chakra-ui/react'
 import ServiceList from '../ServiceList'
 import { useWallets } from '../../hooks/useWallets'
-import { isGreaterThanOrEqualToVersion } from '../../helpers/version'
-import { useConfig } from '../../contexts/FclContext'
-import { AVAILABLE_FEATURES, SUPPORTED_VERSIONS } from '../../helpers/constants'
 import { Wallet } from '../../data/wallets'
 import { useIsCollapsed } from '../../hooks/useIsCollapsed'
-import FilterBar from '../FilterBar'
-import features from '../../data/features'
 import { useState } from 'react'
 
 type Props = {
@@ -29,20 +16,11 @@ export default function WalletSelection({
   onSelectWallet,
   selectedWallet,
 }: Props) {
-  const { wallets } = useWallets()
   const isCollapsed = useIsCollapsed()
-  const [selectedFilters, setSelectedFilters] = useState<string[]>([])
 
   return (
     <Stack spacing={0} flexGrow={1} overflow="hidden">
       <Stack overflow="scroll" px={4} pb={5} flexGrow={1}>
-        {/* TODO: RESTORE
-        <FilterBar
-          availableFilters={AVAILABLE_FEATURES.map(f => f.name)}
-          selectedFilters={selectedFilters}
-          onSelectedFiltersChange={setSelectedFilters}
-        />*/}
-
         <ServiceList
           onSelectWallet={onSelectWallet}
           selectedWallet={selectedWallet}
