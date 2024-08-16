ae557c0ff6299c384fe76f367cfe544bf01805f8
Jordan Ribbink <jribbink@telus.net>
2024-08-16 13:57:03 -0700

    test different cors

--- next.config.js
+++ next.config.js
@@ -26,44 +26,6 @@ const nextJsConfig = {
   env: {
     SENTRY_DSN: process.env.SENTRY_DSN,
   },
-  async headers() {
-    return [
-      {
-        // Allow CORS for authn routes
-        source: '/api/:network/authn',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-      {
-        // Allow CORS for /api/authn
-        source: '/api/authn',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,DELETE,PATCH,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 // Add Sentry Config
