0c78a7e252c3491eda4adc23b5b47a6f0954aae3
Jordan Ribbink <jribbink@telus.net>
2024-08-20 16:04:45 -0700

    Fix collpased view flicker

--- hooks/useIsCollapsed.ts
+++ hooks/useIsCollapsed.ts
@@ -1,9 +1,18 @@
 import { useBreakpointValue } from '@chakra-ui/react'
+import { useDevice } from '../contexts/DeviceContext'
+import { DeviceType } from '../helpers/device'
 
 export function useIsCollapsed() {
+  const { deviceInfo } = useDevice()
   const isCollapsed = useBreakpointValue(
     { base: true, md: false },
-    { ssr: true, fallback: 'base' },
+    {
+      ssr: true,
+      fallback:
+        deviceInfo.type === DeviceType.MOBILE && !deviceInfo.isTablet
+          ? 'md'
+          : 'base',
+    },
   )
   return isCollapsed
 }
